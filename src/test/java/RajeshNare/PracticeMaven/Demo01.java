0fd4c1993e2f11d98723c20de9c7566af8b4e97f
rajeshDnare <rajeshnare@gmail.com>
2022-01-21 21:20:47 +0530

    again started

---
+++ src/test/java/RajeshNare/PracticeMaven/Demo01.java
@@ -0,0 +1,10 @@
+package RajeshNare.PracticeMaven;
+
+import org.testng.annotations.Test;
+
+public class Demo01 {
+	@Test
+	public void product() {
+		System.out.println("This is just practice");
+	}
+}
